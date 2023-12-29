36c613576792cc454b1813c65c1f1e0b51ca3873
Spaceginner <ivan.demian2009@gmail.com>
2023-12-29 13:17:15 +0200

    keep track of what migrations were already applied

--- purrcafe/_database/__init__.py
+++ purrcafe/_database/__init__.py
@@ -7,5 +7,16 @@ from ._sessions import Session
 from ._files import File
 
 
-with database_lock.writer:
-    complete_migrations(database, Path(__file__).parent.joinpath("migrations"))
+def _complete_migrations():
+    migrations_path = Path(__file__).parent.joinpath("migrations")
+
+    state_file = migrations_path.joinpath("state")
+    last_migration = int(state_file.read_text()) if state_file.exists() else -1
+
+    with database_lock.writer:
+        final_migration = complete_migrations(database, migrations_path, last_migration)
+
+    state_file.write_text(str(final_migration))
+
+
+_complete_migrations()
--- purrcafe/_database/_utils/_migrations.py
+++ purrcafe/_database/_utils/_migrations.py
@@ -5,14 +5,22 @@ import sqlite3
 from ..._logging import logger
 
 
-def complete_migrations(database: sqlite3.Connection, migrations_path: PathLike | str | bytes) -> None:
+def complete_migrations(database: sqlite3.Connection, migrations_path: PathLike | str | bytes, last_migration: int) -> int:
     migrations_path = Path(migrations_path)
 
     logger.debug(f"applying migrations from {migrations_path}")
 
+    final_migration = last_migration
     for name, script in map(lambda path: (path.name, path.read_text()), sorted(filter(lambda path: path.is_file() and path.suffix == '.sql', migrations_path.iterdir()), key=lambda path: path.name)):
+        if (final_migration := int(name.split('_', 1)[0])) <= last_migration:
+            logger.debug(f"skipping '{name}' migration...")
+
+            continue
+
         logger.info(f"applying '{name}' migration...")
         database.executescript(script)
         database.commit()
 
     logger.info("finished migrations")
+
+    return final_migration
