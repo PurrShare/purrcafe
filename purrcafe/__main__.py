c2b927114c2053a1e681a3212a78d6034edaddd5
Spaceginner <ivan.demian2009@gmail.com>
2023-12-29 12:36:10 +0200

    log tracebacks by uvicorn

--- purrcafe/__main__.py
+++ purrcafe/__main__.py
@@ -9,5 +9,5 @@ uvicorn.run(
     app,
     host="0.0.0.0" if os.environ.get('PURRCAFE_LISTEN') == '1' else "localhost",
     port=int(os.environ.get('PURRCAFE_PORT', 8080)),
-    log_level=os.environ.get('PURRCAFE_UVICORN_LOGLEVEL', "critical")
+    log_level=os.environ.get('PURRCAFE_UVICORN_LOGLEVEL', "error")
 )
